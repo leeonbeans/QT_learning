a1d870e56d7843ec3a9077465c4f47f95a595f9a
Leeon <weiibeansd@gmail.com>
2024-12-08 20:48:46 +0800

    Pages now can change and go back

--- CaseHistoryManager/masterview.h
+++ CaseHistoryManager/masterview.h
@@ -2,6 +2,12 @@
 #define MASTERVIEW_H
 
 #include <QWidget>
+#include "loginview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patienteditview.h"
+#include "patientview.h"
+#include "welcomeview.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +23,28 @@ public:
     MasterView(QWidget *parent = nullptr);
     ~MasterView();
 
+public slots:
+    void goLoginView();
+    void goWelcomeView();
+    void goDoctorView();
+    void goDepartmentView();
+    void goPatientEditView();
+    void goPatientView();
+    void goPreviousView();
+
+private slots:
+    void on_btnBack_clicked();
+
 private:
+    void pushWidgetToStackView(QWidget *widget);
+
     Ui::MasterView *ui;
+
+    WelcomeView *welcomeView;
+    DoctorView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
 };
 #endif // MASTERVIEW_H
